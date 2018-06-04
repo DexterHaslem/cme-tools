8ca559ee331bd1ed7e4e0f160b56af8b4ec210aa
Dexter Haslem <dmh@fastmail.com>
2018-06-04 00:30:08 -0600

    option vol scanner thing

--- hotstrikes.py
+++ hotstrikes.py
@@ -1,8 +1,5 @@
 from csv import DictReader
 from urllib.request import urlopen
-# use clock over time on windows, will use RTC hardware
-from time import clock
-
 from netutil import get_network_lines
 
 FTP_ROOT = 'ftp://ftp.cmegroup.com/pub/settle/'
@@ -15,9 +12,43 @@ def get_all_options_gen():
         with urlopen(url) as resp:
             reader = DictReader(get_network_lines(resp))
             for row in reader:
+                # we do all our processing by volume, so parse it here
+                volstr = row['EST. VOL']
+                vol = int(volstr)
+                row['EST. VOL'] = vol
                 yield row
 
 
+if __name__ == '__main__':
+    print('fetching all options for COMEX and NYMEX, this will take a few seconds..')
+    options = list(get_all_options_gen())
+    print('got {} options and strikes'.format(len(options)))
 
-if __name__ == "__main__":
-    pass
+    print('filtering active')
+
+    # grab only strikes with trading activity of decent mention
+
+    # TODO: process these in one loop. pre-processing active is small enough it doesnt effect perf currently ...
+    # but pulling in equity products would be devastating
+    active = [o for o in options if o['EST. VOL'] >= 100]
+    calls = [o for o in active if o['PUT/CALL'] == 'C']
+    puts = [o for o in active if o['PUT/CALL'] == 'P']
+
+    sorted_calls = sorted(calls, key=lambda v: v['EST. VOL'], reverse=True)
+    sorted_puts = sorted(puts, key=lambda v: v['EST. VOL'], reverse=True)
+
+
+    def print_opt(o):
+        print('\tPROD={:<4} STRIKE={:<6} VOL={:<6} SETTLE={:<8}'.format(o['PRODUCT SYMBOL'], o['STRIKE'], o['EST. VOL'],
+                                                                        o['SETTLE']))
+
+
+    # map(print_opt, sorted_calls[:10])
+
+    print('** top calls by vol**')
+    for v in sorted_calls[:10]:
+        print_opt(v)
+
+    print('** top puts by vol**')
+    for v in sorted_puts[:10]:
+        print_opt(v)
